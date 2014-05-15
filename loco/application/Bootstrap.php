119a0341798b42e39e82fc09ea8ba16606cf8263
4mp3R <p4r0l1990>
2014-05-15 22:43:21 +0200

    Layouts, stylesheet and htaccess configured
    2 layouts (public and private) added and set up.
    Some head pleaceholders that will be used in layout set up in Bootstrap.
    ini file configured for work with layout and database
    Stylesheet added, htaccess edited in order to permit to browsers to
    download content (such as css and js) from the public folder

--- loco/application/Bootstrap.php
+++ loco/application/Bootstrap.php
@@ -1,8 +1,29 @@
 <?php
 
+//All methods that begins with _ will be executed automatically at bootstrap time
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected $_view;
 
+    //need to initialize FrontController here in order to be able to use baseUrl() at the bootstrap time
+    //baseUrl() return the path to a /public folder accessible by browsers
+    protected function _initRequest()
+    {
+        $this->bootstrap('FrontController');
+        $front = $this->getResource('FrontController');
+        $request = new Zend_Controller_Request_Http();
+        $front->setRequest($request);
+    }
 
+    //initialize some placeholders that will be used in templates
+    protected function _initViewSettings()
+    {
+        $this->bootstrap('view');
+        $this->_view = $this->getResource('view');
+        $this->_view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=UTF-8');
+        $this->_view->headMeta()->appendHttpEquiv('Content-Language', 'it-IT');
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/style.css'));
+        $this->_view->headTitle('Loco!');
+    }
 }
 
